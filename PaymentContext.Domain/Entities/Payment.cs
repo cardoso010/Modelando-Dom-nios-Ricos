a2ec6c7fe96d8f4ddb733909ff17283d5b14202e
Gabriel Cardoso Luiz <gabriel.cardoso010@gmail.com>
2019-05-07 12:45:22 -0300

    Design By Contracts

--- PaymentContext.Domain/Entities/Payment.cs
+++ PaymentContext.Domain/Entities/Payment.cs
@@ -1,9 +1,11 @@
 using System;
+using Flunt.Validations;
 using PaymentContext.Domain.ValueObjects;
+using PaymentContext.Shared.Entities;
 
 namespace PaymentContext.Domain.Entities
 {
-    public abstract class Payment
+    public abstract class Payment : Entity
     {
         protected Payment(string number, DateTime paidDate, DateTime expireDate, decimal total, decimal totalPaid, string owner, Document document, Address address, Email email)
         {
@@ -16,6 +18,12 @@ namespace PaymentContext.Domain.Entities
             Document = document;
             Address = address;
             Email = email;
+
+            AddNotifications(new Contract()
+                .Requires()
+                .IsGreaterThan(0, Total, "Payment.Total", "O total nao pode ser 0")
+                .IsGreaterOrEqualsThan(Total, TotalPaid, "Payment.TotalPaid", "O valor pago é menor do que o valor do pagamento")
+            );
         }
 
         public string Number { get; private set; }
--- PaymentContext.Domain/ValueObjects/Address.cs
+++ PaymentContext.Domain/ValueObjects/Address.cs
@@ -1,3 +1,4 @@
+using Flunt.Validations;
 using PaymentContext.Domain.Enums;
 using PaymentContext.Shared.ValueObjects;
 
@@ -14,6 +15,11 @@ namespace PaymentContext.Domain.ValueObjects {
             State = state;
             Country = country;
             ZipCode = zipCode;
+
+            AddNotifications(new Contract()
+                .Requires()
+                .HasMinLen(Street, 3, "Address.Street", "A rua deve conter pelo menos 3 caracteres")
+            );
         }
 
         public string Street { get; private set; }
